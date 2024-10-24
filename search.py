e7a2e0fd39c784f5e3aaf932384f28c7101a310e
Tmin1021 <ngngocduytan8604@gmail.com>
2024-10-25 05:44:57 +0700

    DFS implement

--- search.py
+++ search.py
@@ -55,3 +55,53 @@ def breadth_first_search(grid, start, stones, switches):
                 frontier.append((new_state, new_path))  
 
     return None, None, None, None
+
+def depth_first_search(grid, start, stones, switches):
+    state = (start, tuple(stones))
+    explored = set() 
+    frontier = [(state, "")] 
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
+    move_mapping = {
+        (-1, 0): "u", (1, 0): "d", (0, -1): "l", (0, 1): "r", 
+        (-1, 0, "push"): "U", (1, 0, "push"): "D", (0, -1, "push"): "L", (0, 1, "push"): "R"
+    }
+    step = 0
+    tracemalloc.start()  
+    start_time = time.time() 
+    print("DFS")
+    
+    while frontier:
+        current_state, path = frontier.pop()
+        char_pos, stone_positions = current_state
+        
+        if is_goal_state(stone_positions, switches):
+            end_time = time.time()
+            memory_used = tracemalloc.get_traced_memory()[1] / (1024 ** 2)  
+            tracemalloc.stop()
+            return step, path, len(explored), end_time - start_time, memory_used
+
+        if current_state in explored:
+            continue
+        explored.add(current_state)
+        
+        for direction in directions:
+            new_char_pos = (char_pos[0] + direction[0], char_pos[1] + direction[1])
+
+            if new_char_pos in stone_positions:
+                stone_index = stone_positions.index(new_char_pos)
+                new_stone_pos = (new_char_pos[0] + direction[0], new_char_pos[1] + direction[1])
+
+                if is_valid_push(char_pos, new_char_pos, direction, grid, stone_positions):
+                    new_stone_positions = list(stone_positions)
+                    new_stone_positions[stone_index] = new_stone_pos  
+                    new_state = (new_char_pos, tuple(new_stone_positions))  
+                    new_path = path + move_mapping[(direction[0], direction[1], "push")]
+                    frontier.append((new_state, new_path))
+
+            elif is_valid_move(new_char_pos, grid, stone_positions):
+                new_state = (new_char_pos, stone_positions)  
+                new_path = path + move_mapping[(direction[0], direction[1])]
+                step += 1 
+                frontier.append((new_state, new_path)) 
+
+    return None, None, None, None
